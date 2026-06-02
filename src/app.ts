3ecaee4e2038c6f0c1bdf8b18bc1ed89c74b13f4
Yeu <yeumensilva9@gmail.com>
2026-06-02 16:51:41 -0300

    handle error

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,23 @@
-import expres from "express";
+import express from "express";
 import cors from "cors";
+import userRouter from "./features/users/user.routes.js";
+import { NotFoundError } from "./shared/errors/app-error.js";
+import { errorHandler } from "./shared/middlewares/error-handler.js";
 
-const app = expres();
-app.use(expres.json());
+const app = express();
+app.use(express.json());
 app.use(cors());
+
+app.use("/api/users", userRouter);
+
+app.all("*", (req, _res, next) => {
+  next(
+    new NotFoundError(
+      `No se puede encontrar ${req.originalUrl} en este servidor.`,
+    ),
+  );
+});
+
+app.use(errorHandler);
 
 export default app;
